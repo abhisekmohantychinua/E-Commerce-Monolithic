913a0da0d9db1a9af745f062df42918591ccfa70
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-12-27 10:48:19 +0530

    Initialize backend

---
+++ e-commerce-backend/src/main/java/dev/abhisek/server/ECommerceBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.abhisek.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceBackendApplication.class, args);
+    }
+
+}

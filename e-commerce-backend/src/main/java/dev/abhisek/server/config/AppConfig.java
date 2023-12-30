53fc9ade657473d0db6db0f814634c532b21742d
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2023-12-30 22:41:54 +0530

    CORS configuration

---
+++ e-commerce-backend/src/main/java/dev/abhisek/server/config/AppConfig.java
@@ -0,0 +1,26 @@
+package dev.abhisek.server.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AppConfig {
+
+    @Value("${client-app.url}")
+    private String clientUrl;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(clientUrl)
+                        .allowedMethods("*");
+            }
+        };
+    }
+}

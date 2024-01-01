01bebabe50e2732e9921ec7684a44b0cafc70230
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-01-02 00:14:47 +0530

    updated

--- e-commerce-frontend/src/app/app.routes.ts
+++ e-commerce-frontend/src/app/app.routes.ts
@@ -1,3 +1,14 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {SearchComponent} from "./pages/search/search.component";
+import {CartsComponent} from "./pages/carts/carts.component";
+import {OrdersComponent} from "./pages/orders/orders.component";
+import {ProfileComponent} from "./pages/profile/profile.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: "full"},
+  {path: 'search',component:SearchComponent, pathMatch:"full"},
+  {path:'carts',component: CartsComponent,pathMatch:"full"},
+  {path:'orders',component: OrdersComponent,pathMatch:"full"},
+  {path:'profile',component: ProfileComponent,pathMatch:"full"},
+];

57c9d3f24d490de65c3c6df78d2bca4e3a0b7af8
Abhisek Mohanty <abhisekmohantychinua@gmail.com>
2024-01-28 10:17:14 +0530

    updated layout

--- e-commerce-frontend/src/app/pages/admin/admin-layout/admin-layout.component.ts
+++ e-commerce-frontend/src/app/pages/admin/admin-layout/admin-layout.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {MatTooltipModule} from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-admin-layout',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    RouterOutlet,
+    MatTooltipModule,
+    RouterLink
+  ],
   templateUrl: './admin-layout.component.html',
   styleUrl: './admin-layout.component.css'
 })
